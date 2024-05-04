dcc55f6423f7d1c827a5e29fcfb2adf66ee7e8eb
Alex <kim.av9@gmail.com>
2024-05-04 21:54:03 +0500

    test

--- conftest.py
+++ conftest.py
@@ -1,15 +1,14 @@
+from selenium import webdriver
 import pytest
 import random
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+
 
 @pytest.fixture
 def browser():
-    chrome_browser = webdriver.Chrome(ChromeDriverManager().install())
+    chrome_browser = webdriver.Chrome()
     chrome_browser.implicitly_wait(10)
     chrome_browser.maximize_window()
     return chrome_browser
-
 
 
 def randomize_list(self):
