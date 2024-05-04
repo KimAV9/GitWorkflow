7770f45a585f0ba986810fb6d94cb35c8ccd343e
Alex <kim.av9@gmail.com>
2024-05-04 22:02:05 +0500

    test

--- conftest.py
+++ conftest.py
@@ -1,11 +1,14 @@
 from selenium import webdriver
 import pytest
 import random
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture
 def browser():
-    chrome_browser = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')
+    chrome_browser = webdriver.Chrome(options=options)
     chrome_browser.implicitly_wait(10)
     chrome_browser.maximize_window()
     return chrome_browser
